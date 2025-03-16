e3ae588f0e3a82c3972f85957521fd001e6eb513
Evgeny Demchenko <v1mpire@icloud.com>
2025-03-16 11:16:11 +0300

    add new city - NN

--- app/service/geo.py
+++ app/service/geo.py
@@ -8,14 +8,21 @@ type latitude = float
 type longitude = float
 type fullname = str
 
+type geo_info = tuple(latitude, longitude, fullname)
+
 
 class GeoCity(Enum):
-    KRD: tuple(latitude, longitude, fullname) = (
+    KRD: geo_info = (
         45.04484,
         38.97603,
         "Krasnodar",
     )
-    MSK: tuple(latitude, longitude, fullname) = (
+    NN: geo_info = (
+        56.3333,
+        44.0000,
+        "Nizhny Novgorod",
+    )
+    MSK: geo_info = (
         55.75222,
         37.61556,
         "Moscow",
