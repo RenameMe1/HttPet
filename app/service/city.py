edb9ea4df4eab1675ce87471fd451db417c8ed53
Evgeny Demchenko <v1mpire@icloud.com>
2025-03-16 11:10:44 +0300

    city wether return city fillname now

--- app/service/city.py
+++ app/service/city.py
@@ -1,13 +1,17 @@
 """Module work on weather."""
+
 from __future__ import annotations
 
-from typing import Protocol
-import asyncio
+from typing import Protocol, TYPE_CHECKING
 
 import aiohttp
 
-from app.service.geo import GeoCity, lat_type, lon_type
-from app.service.env import env
+from app.service.geo import GeoCity
+from app.env import env
+
+if TYPE_CHECKING:
+    import typing
+
 
 class CityProtocol(Protocol):
     def __init__(self, city: GeoCity): ...
@@ -16,37 +20,31 @@ class CityProtocol(Protocol):
 
 class City:
     """Class work with API openweathermap.org."""
+
     __slots__ = [
-        'city',
+        "city",
     ]
 
-    weather_api: typing.ClassVar = 'https://api.openweathermap.org/data/2.5/weather'
+    weather_api: typing.ClassVar = "https://api.openweathermap.org/data/2.5/weather"
     api_key: typing.ClassVar = env.API_OPENWEATHER_KEY
 
     def __init__(self, city: GeoCity):
         self.city = city
 
-    async def get_current_weather(self):
-
-        lat, lon = self.city.value
-     
-        return await self._request_weather_by_geo(lat, lon)
-
-    async def _request_weather_by_geo(self, lat: lat_type, lon: lon_type):
+    async def get_current_weather(self) -> tuple[str, str]:
+        lat, lon, city_name = self.city.value
 
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                    self.weather_api,
-                    params = [
-                            ('lat', lat), 
-                            ('lon', lon),
-                            ('appid', self.api_key),
-                            ],
-                    ) as response:
-
+                self.weather_api,
+                params=[
+                    ("lat", lat),
+                    ("lon", lon),
+                    ("appid", self.api_key),
+                ],
+            ) as response:
                 response = await response.json()
 
-                weather = response['weather'][0]
+        weather = response["weather"][0]
 
-                return weather['description']
-
+        return (weather["description"], city_name)
