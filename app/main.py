0cd2b277e69e60653b80b2561455952088ec64c6
Evgeny Demchenko <v1mpire@icloud.com>
2025-03-17 11:27:20 +0000

    ruff warning update

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ import asyncio
 from app.controllers.output import ExcelOutput
 from app.service.general_report import GeneralReportService
 
+
 async def main():
     excel = ExcelOutput("output.xlsx")
     report = GeneralReportService(output=excel)
 
     await report.generate()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
