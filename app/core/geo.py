9fa968c94dfb6e4748b463e6cac546783d6a82e8
Evgeny Demchenko <v1mpire@icloud.com>
2025-03-18 12:02:07 +0000

    add new city

--- app/core/geo.py
+++ app/core/geo.py
@@ -21,3 +21,58 @@ class GeoCity(Enum):
         37.61556,
         "Moscow",
     )
+    PTGRSK = (
+        44.05,
+        43.06,
+        "Pyatigorsk",
+    )
+    RJEV = (
+        56.33,
+        36.73,
+        "Rjev",
+    )
+    LPTSK = (
+        52.6,
+        39.57,
+        "Lipetsk",
+    )
+    MGDN = (
+        59.56,
+        150.8,
+        "Magadan",
+    )
+    SNJ = (
+        43.32,
+        45.05,
+        "Soonja",
+    )
+    MKOP = (
+        44.61,
+        40.11,
+        "Maykop",
+    )
+    MKHCHL = (
+        42.98,
+        47.5,
+        "Makhachkala",
+    )
+    VLDMR = (
+        56.14,
+        40.4,
+        "Vladimir",
+    )
+    VLGD = (
+        59.22,
+        39.88,
+        "Vologda",
+    )
+    VKSA = (
+        55.32,
+        42.17,
+        "Vyksa",
+    )
+    GTCHNA = (
+        59.58,
+        30.13,
+        "Gatchina",
+    )
