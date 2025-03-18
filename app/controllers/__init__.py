84ac81fe199b6ae6ab96b88ec3a9d769af361063
Evgeny Demchenko <v1mpire@icloud.com>
2025-03-18 10:23:48 +0000

    repair import errors

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,10 +1,10 @@
 """Module work on the non-processor dependencies."""
 
 from app.controllers.city import CityController, ICityController
-from app.controllers.News import INewsController, NewsapiController
+from app.controllers.news import INewsController, NewsapiController
 from app.controllers.output import IOutputController, ExcelController
-from app.controllers.users import IUserController, UserController
-from app.columns.env import env
+from app.controllers.user import IUserController, UserController
+from app.controllers.env import env
 
 __all__ = [
     "CityController",
