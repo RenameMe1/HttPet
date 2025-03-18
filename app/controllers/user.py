4a4fa2e6f070e885dbcff2816220231c58a5449d
Evgeny Demchenko <v1mpire@icloud.com>
2025-03-18 08:33:24 +0000

    add user module

---
+++ app/controllers/user.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import Protocol, TYPE_CHECKING, final
+
+import aiohttp
+
+if TYPE_CHECKING:
+    pass
+
+__all__ = (
+    "IUserController",
+    "UserController",
+)
+
+
+class IUserController(Protocol):
+    def get_users(self, amount: int): ...
+
+
+@final
+class UserController:
+    """Class work on API https://randomuser.me/."""
+
+    user_api = "https://randomuser.me/api"
+
+    async def get_users(self, *, amount: int) -> dict:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                self.user_api,
+                params=[
+                    ("results", amount),
+                ],
+            ) as response:
+                response = await response.json()
+
+        users = response.get("results")
+
+        if users is None:
+            return {}
+
+        return users
